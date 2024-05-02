9a5a2c0cf436c50e73ec6c7f5a2e6a1cceaca821
Oswaldo Valles <oswyvalles@gmail.com>
2024-05-02 08:17:38 -0600

    Ejercicio 5: Hacer un programa que imprima los números impares hasta el 100 y que imprima cuantos impares hay.

---
+++ 5.js
@@ -0,0 +1,9 @@
+// EJERCICIO 5
+let impares = 0;
+
+for (let i = 1; i <= 100; i += 2) {
+    console.log(i);
+    impares++;
+}
+
+console.log("El total de numeros impares hasta el 100 es de:", impares);
